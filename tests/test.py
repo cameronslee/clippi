f0a44e62c7304f576c32fe7e36d94433c6b0db9e
Cameron Lee <cameronslee@hotmail.com>
2024-05-14 11:55:01 -0700

    fix: main script import

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
-from clippy import get_transcript
+from clippi import get_transcript
 
 def test_setup():
     assert 2 == 2
