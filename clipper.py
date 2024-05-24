c64fe1983e3a7933099a1e0abb08e2f40ea4fed5
Cameron Lee <cameronslee@hotmail.com>
2024-05-23 19:15:49 -0700

    refactor: mss w/ kadanes algow

--- clipper.py
+++ clipper.py
@@ -10,35 +10,47 @@ if platform.system() == 'Darwin':
     os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/bin/ffmpeg"
 
 # the heart and soul of the clipping algorithm: a vectorized version of maximum subarray sum
-def mss(arr):
+def mss(df):
     res = []
-    for i in range(len(arr)):
-        sum = 0
-        for j in range (i,len(arr)):
-            sum += arr[j]
-            res.append((sum, i, j, j-i))
+    curr_max = float('-inf')
+    global_max = 0
+    #curr_start_time = df['start_time'][0]
+    #curr_end_time = df['end_time'][0]
+    curr_start_time = 0
+    curr_end_time = 0
 
-    res.sort(reverse=True)
-    return res
+    for index, row in df.iterrows():
+        global_max += row['weight']
 
-def make_clips(video_file, input_file, output_file, output_dir):
+        if global_max > curr_max:
+            curr_max = global_max
+            curr_end_time = row['end_time']
+            res.append((curr_start_time, curr_end_time, (curr_end_time - curr_start_time), global_max))
+
+        if global_max < 0:
+            global_max = 0
+            curr_start_time = row['start_time']
+
+    return sorted(res, key=lambda element: (element[2], element[3]), reverse=True)
+
+def make_clips(video_file, input_file, output_dir):
     try:
         df = pd.read_csv(input_file)
     except:
         perror("could not read " + input_file)
         exit(1)
-
     try:
-        weights = df['weight'].tolist()
-        print("mss length: ", len(mss(weights)))
-        clip = mss(weights)[0]
-        print(df['start_time'][clip[1]], df['end_time'][clip[2]])
+        clips = mss(df)
+        print(clips)
     except:
-        perror("could not generate clip") 
+        perror("could not calculate clip weights") 
         exit(1)
 
-    v = VideoFileClip(video_file)
-
-    best_clip = v.subclip(df['start_time'][clip[1]], df['end_time'][clip[2]])
-    best_clip.write_videofile(output_dir+output_file,  codec="libx264", audio_codec="aac")
-
+    try:
+        v = VideoFileClip(video_file)
+        for i in range(len(clips)):
+            curr = v.subclip(clips[i][0], clips[i][1])
+            curr.write_videofile(output_dir+str(i)+"_"+str(clips[i][3])+".mp4", codec="libx264", audio_codec="aac")
+    except:
+        perror("could not generate clips")
+        exit(1)
