0a318c0ad491cfb9e36cae55cd257f7f11b1e5f5
Cameron Lee <cameronslee@hotmail.com>
2024-05-25 10:29:38 -0700

    refactor: tests updated

--- tests/test_clipper.py
+++ tests/test_clipper.py
@@ -5,15 +5,22 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
+from helpers import mkdir
 from clipper import make_clips 
 
-PREPROCESSING_OUTPUT_DIR = "./"
-TEST_INPUT_1 = "out_weighted.csv"
-OUTPUT_DIR = './clips/'
+from conf import TEST_DIR
 
-VIDEO = 'What Mark Zuckerberg learned from Caesar Augustus.mp4'
+if len(sys.argv) != 3:
+    print("test_clipper.py <video> <input-file>")
+    exit(1)
+
+CLIPPER_OUTPUT_DIR = TEST_DIR + "./clipper_output"
+mkdir(CLIPPER_OUTPUT_DIR)
+TEST_VIDEO = sys.argv[1]
+TEST_INPUT = sys.argv[2]
+
 
 def test_make_clips():
-    res = make_clips(video_file=VIDEO, input_file=TEST_INPUT_1, output_dir=OUTPUT_DIR, lower_bound=5.0, upper_bound=15.0, debug_flag1=True)
+    res = make_clips(video_file=TEST_VIDEO, input_file=TEST_INPUT, output_dir=CLIPPER_OUTPUT_DIR, lower_bound=5.0, upper_bound=15.0, debug_flag1=True)
     assert len(res) != 0
     
